bfa7c0693f0581d9a2a0e5e3bccf3fa7a216d1d6
yeonjaeim <yeonjae.im@hva.nl>
2023-12-07 15:51:41 +0900

    change BASE_DIR to use os.path.join

--- pollsapi/settings.py
+++ pollsapi/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,7 +81,7 @@ WSGI_APPLICATION = 'pollsapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
